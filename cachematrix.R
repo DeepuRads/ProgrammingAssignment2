e55243ae783562663e148eb04917715f1d9a177f
DeepuRads <30325191+DeepuRads@users.noreply.github.com>
2017-11-12 20:19:15 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions create a special object that stores
+## an invertible square matrix and cache's its inverse.
 
-## Write a short comment describing this function
+
+## This function creates a special "matrix" object that is a
+## a list containing the functions to 
+## 1.   set the value of the vector
+## 2.   get the value of the vector
+## 3.   set the value of the mean
+## 4.   get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special
+## "matrix" created with the above function. However, it first
+## checks to see if the inverse has already been calculated.
+## If so, it gets the inverse from the cache and skips the
+## computation. Otherwise, it calculates the inverse of the
+## matrix and sets the value of the inverse in the cache via
+## the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
